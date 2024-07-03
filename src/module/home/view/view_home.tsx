ee5090819464d046da0a7ad591a1a2546f3cd695
lukman <lukman@lukmans-MacBook-Air.local>
2024-07-03 17:32:20 +0800

    style : update home
    Deskripsi:
    - add view detail feature
    - view home
    
    No issue

--- src/module/home/view/view_home.tsx
+++ src/module/home/view/view_home.tsx
@@ -5,16 +5,18 @@ import React from 'react';
 import { HiMagnifyingGlass, HiOutlineBell, HiOutlineUser } from "react-icons/hi2";
 import Carosole from '../components/carosole';
 import Features from '../components/features';
+import { useRouter } from 'next/navigation';
 
 
 export default function ViewHome() {
+  const router = useRouter()
   return (
     <>
         <LayoutNavbarHome>
           <Group justify='space-between'>
             <Text fw={'bold'} c={'white'} >Perbekal Darmasaba</Text>
             <Group>
-              <ActionIcon variant="light" bg={WARNA.bgIcon} size="lg" radius="lg" aria-label="Settings">
+              <ActionIcon onClick={() => router.push('/search')} variant="light" bg={WARNA.bgIcon} size="lg" radius="lg" aria-label="Settings">
                 <HiMagnifyingGlass size={20} color='white' />
               </ActionIcon>
               <ActionIcon variant="light" bg={WARNA.bgIcon} size="lg" radius="lg" aria-label="Settings">
