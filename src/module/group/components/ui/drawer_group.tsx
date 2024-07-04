1d0253c15b5f19bb1c181036988f6a8ec17ed5c1
amel <amel@amel.local>
2024-07-04 17:23:40 +0800

    upd: remove package
    Deskripsi:
    - remove jotai
    
    No Issues

--- src/module/group/components/ui/drawer_group.tsx
+++ src/module/group/components/ui/drawer_group.tsx
@@ -1,7 +1,6 @@
 import { isDrawer, LayoutDrawer, WARNA } from '@/module/_global';
 import { useHookstate } from '@hookstate/core';
 import { Box, Button, Center, Flex, Group, SimpleGrid, Stack, Text, TextInput } from '@mantine/core';
-import { useAtom } from 'jotai';
 import React, { useState } from 'react';
 import { IoAddCircle } from "react-icons/io5";
 
