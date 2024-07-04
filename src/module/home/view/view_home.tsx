75c1dc6ba7d1bbc4c2a8147b172084b691a7da58
amel <amel@amel.local>
2024-07-04 09:59:45 +0800

    upd: tambahan mas malik

--- src/module/home/view/view_home.tsx
+++ src/module/home/view/view_home.tsx
@@ -4,27 +4,29 @@ import React from 'react';
 import { HiMagnifyingGlass, HiOutlineBell, HiOutlineUser } from "react-icons/hi2";
 import Carosole from '../components/carosole';
 import Features from '../components/features';
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 
 
 export default function ViewHome() {
-  const router = useRouter()
+  // const router = useRouter()
   return (
     <>
-        <LayoutNavbarHome>
-          <Group justify='space-between'>
-            <Text fw={'bold'} c={'white'} >Perbekal Darmasaba</Text>
-            <Group>
-              <ActionIcon onClick={() => router.push('/search')} variant="light" bg={WARNA.bgIcon} size="lg" radius="lg" aria-label="Settings">
-                <HiMagnifyingGlass size={20} color='white' />
-              </ActionIcon>
-              <ActionIcon variant="light" bg={WARNA.bgIcon} size="lg" radius="lg" aria-label="Settings">
-                <HiOutlineBell size={20} color='white' />
-              </ActionIcon>
-              <ActionIcon variant="light" bg={WARNA.bgIcon} size="lg" radius="lg" aria-label="Settings">
-                <HiOutlineUser size={20} color='white' />
-              </ActionIcon>
-            </Group>
+      <LayoutNavbarHome>
+        <Group justify='space-between'>
+          <Text fw={'bold'} c={'white'} >Perbekal Darmasaba</Text>
+          <Group>
+            {/* <ActionIcon onClick={() => router.push('/search')} variant="light" bg={WARNA.bgIcon} size="lg" radius="lg" aria-label="Settings">
+              <HiMagnifyingGlass size={20} color='white' />
+            </ActionIcon> */}
+            <ActionIcon variant="light" bg={WARNA.bgIcon} size="lg" radius="lg" aria-label="Settings">
+              <HiOutlineBell size={20} color='white' />
+            </ActionIcon>
+            <ActionIcon variant="light" bg={WARNA.bgIcon} size="lg" radius="lg" aria-label="Settings">
+              <HiOutlineUser size={20} color='white' />
+            </ActionIcon>
+          </Group>
+        </Group>
+      </LayoutNavbarHome>
       <Box p={20}>
         <Stack >
           <Carosole />
