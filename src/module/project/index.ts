4acac52a0ca4490ccf00fa2eee8addc09e328972
lukman <lukman@lukmans-MacBook-Air.local>
2024-07-08 17:30:39 +0800

    style : update project
    Deskripsi:
    - add view create project
    - add view filter project
    
    No issue

--- src/module/project/index.ts
+++ src/module/project/index.ts
@@ -1,3 +1,7 @@
+import ViewCreateProject from "./view/view_create_project";
+import ViewFilterProject from "./view/view_filter_project";
 import ViewProject from "./view/view_project";
 
-export {ViewProject}
+export { ViewProject }
+export { ViewCreateProject }
+export { ViewFilterProject }
