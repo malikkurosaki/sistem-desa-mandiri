0c62724feb6b7fa9d9191acb117c6d359e2b94eb
amel <amel@amel.local>
2024-07-09 10:39:01 +0800

    upd: grup
    Deskripsi:
    - menggunakan layout navbar new
    - menggunakan local state
    - menggunakan modal konfirmasi dan toast
    
    No Issues

--- src/module/group/components/list_group_active.tsx
+++ src/module/group/components/list_group_active.tsx
@@ -1,50 +1,50 @@
-import { isDrawer, LayoutDrawer, WARNA } from '@/module/_global';
+import { LayoutDrawer, WARNA } from '@/module/_global';
 import { ActionIcon, Box, Group, Text, TextInput } from '@mantine/core';
 import React, { useState } from 'react';
 import { HiOutlineOfficeBuilding } from 'react-icons/hi';
 import { HiMagnifyingGlass } from 'react-icons/hi2';
 import EditDrawerGroup from './ui/edit_drawer_group';
-import { useHookstate } from '@hookstate/core';
-import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
 
 const dataGroup = [
   {
     id: 1,
-    name: 'Lembaga Pengkreditan Desa'
+    name: 'Dinas'
   },
   {
     id: 2,
-    name: 'Lembaga Pengkreditan Desa'
+    name: 'Adat'
   },
   {
     id: 3,
-    name: 'Lembaga Pengkreditan Desa'
+    name: 'LPD'
   },
   {
     id: 4,
-    name: 'Lembaga Pengkreditan Desa'
+    name: 'Karang Taruna'
   },
   {
     id: 5,
-    name: 'Lembaga Pengkreditan Desa'
+    name: 'BPD'
   },
   {
     id: 6,
-    name: 'Lembaga Pengkreditan Desa'
+    name: 'LPM'
   },
   {
     id: 7,
-    name: 'Lembaga Pengkreditan Desa'
+    name: 'PKK'
   },
   {
     id: 8,
-    name: 'Lembaga Pengkreditan Desa'
+    name: 'Pengelolaan Penduduk'
   },
 ]
 
 export default function ListGroupActive() {
-  // const openDrawerEdit = useHookstate(isDrawer)
   const [openDrawer, setOpenDrawer] = useState(false)
+  const [valChoose, setValChoose] = useState("")
+
   return (
     <Box pt={20}>
       <TextInput
@@ -63,11 +63,18 @@ export default function ListGroupActive() {
       {dataGroup.map((v, i) => {
         return (
           <Box pt={20} key={i}>
-            <Group align='center' style={{
-              border: `1px solid ${"#DCEED8"}`,
-              padding: 10,
-              borderRadius: 10
-            }} onClick={() => setOpenDrawer(true)} >
+            <Group align='center'
+              style={{
+                border: `1px solid ${"#DCEED8"}`,
+                padding: 10,
+                borderRadius: 10,
+                cursor: 'pointer'
+              }}
+              onClick={() => {
+                setValChoose(v.name)
+                setOpenDrawer(true)
+              }}
+            >
               <Box>
                 <ActionIcon variant="light" bg={'#DCEED8'} size={50} radius={100} aria-label="icon">
                   <HiOutlineOfficeBuilding color={WARNA.biruTua} size={25} />
@@ -80,8 +87,13 @@ export default function ListGroupActive() {
           </Box>
         )
       })}
-      <LayoutDrawer opened={openDrawer} onClose={() => setOpenDrawer(false)} title="LEMBAGA PENGKREDITAN DESA">
-        <EditDrawerGroup />
+      <LayoutDrawer opened={openDrawer} onClose={() => setOpenDrawer(false)} title={valChoose}>
+        <EditDrawerGroup onUpdated={(val) => {
+          if (val) {
+            toast.success('Sukses! data tersimpan')
+          }
+          setOpenDrawer(false)
+        }} />
       </LayoutDrawer>
     </Box>
   );
--- src/module/group/components/list_group_non_active.tsx
+++ src/module/group/components/list_group_non_active.tsx
@@ -1,47 +1,50 @@
-import { isDrawer, LayoutDrawer, WARNA } from '@/module/_global';
+import { LayoutDrawer, WARNA } from '@/module/_global';
 import { ActionIcon, Box, Group, Text, TextInput } from '@mantine/core';
 import React, { useState } from 'react';
 import { HiOutlineOfficeBuilding } from 'react-icons/hi';
 import { HiMagnifyingGlass } from 'react-icons/hi2';
 import EditDrawerGroup from './ui/edit_drawer_group';
+import toast from 'react-hot-toast';
 
 const dataGroup = [
   {
     id: 1,
-    name: 'No Active Desa'
+    name: 'Dinas'
   },
   {
     id: 2,
-    name: 'No Active Desa'
+    name: 'Adat'
   },
   {
     id: 3,
-    name: 'No Active Desa'
+    name: 'LPD'
   },
   {
     id: 4,
-    name: 'No Active Desa'
+    name: 'Karang Taruna'
   },
   {
     id: 5,
-    name: 'No Active Desa'
+    name: 'BPD'
   },
   {
     id: 6,
-    name: 'No Active Desa'
+    name: 'LPM'
   },
   {
     id: 7,
-    name: 'No Active Desa'
+    name: 'PKK'
   },
   {
     id: 8,
-    name: 'No Active Desa'
+    name: 'Pengelolaan Penduduk'
   },
 ]
 
 export default function ListGroupNonActive() {
   const [openDrawer, setOpenDrawer] = useState(false)
+  const [valChoose, setValChoose] = useState("")
+
   return (
     <Box pt={20}>
       <TextInput
@@ -60,11 +63,18 @@ export default function ListGroupNonActive() {
       {dataGroup.map((v, i) => {
         return (
           <Box pt={20} key={i}>
-            <Group align='center' style={{
-              border: `1px solid ${"#DCEED8"}`,
-              padding: 10,
-              borderRadius: 10
-            }} onClick={() => setOpenDrawer(true)}>
+            <Group align='center'
+              style={{
+                border: `1px solid ${"#DCEED8"}`,
+                padding: 10,
+                borderRadius: 10,
+                cursor: 'pointer'
+              }}
+              onClick={() => {
+                setValChoose(v.name)
+                setOpenDrawer(true)
+              }}
+            >
               <Box>
                 <ActionIcon variant="light" bg={'#DCEED8'} size={50} radius={100} aria-label="icon">
                   <HiOutlineOfficeBuilding color={WARNA.biruTua} size={25} />
@@ -77,8 +87,13 @@ export default function ListGroupNonActive() {
           </Box>
         )
       })}
-      <LayoutDrawer opened={openDrawer} onClose={() => setOpenDrawer(false)} title="LEMBAGA PENGKREDITAN DESA">
-        <EditDrawerGroup />
+      <LayoutDrawer opened={openDrawer} onClose={() => setOpenDrawer(false)} title={valChoose}>
+        <EditDrawerGroup onUpdated={(val) => {
+          if (val) {
+            toast.success('Sukses! data tersimpan')
+          }
+          setOpenDrawer(false)
+        }} />
       </LayoutDrawer>
     </Box>
   );
