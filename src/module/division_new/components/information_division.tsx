701ad84085dd509aaff714a9bf5df65447e77e45
lukman <lukman@lukmans-MacBook-Air.local>
2024-07-17 17:41:29 +0800

    style : update information

--- src/module/division_new/components/information_division.tsx
+++ src/module/division_new/components/information_division.tsx
@@ -16,7 +16,7 @@ export default function InformationDivision() {
             border: `1px solid ${WARNA.borderBiruMuda}`,
             borderRadius: 10
           }}>
-            <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </Text>
+            <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </Text>
           </Box>
         </Box>
       </Box>
