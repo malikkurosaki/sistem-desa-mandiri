f5ee1bfed20270f9cc8b1780fd5f69e595196a83
amel <amel@amel.local>
2024-07-18 14:30:30 +0800

    upd: member
    Deskripsi:
    - tab status member
    - edit form
    - informasi gender
    
    No Issues

--- src/module/user/member/component/ui/navbar_detail_member.tsx
+++ src/module/user/member/component/ui/navbar_detail_member.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { LayoutNavbarHome, LayoutIconBack, WARNA, LayoutDrawer } from "@/module/_global";
-import { Box, Group, ActionIcon, Stack, Text } from "@mantine/core";
+import { Box, Group, ActionIcon, Stack, Text, Center, Avatar } from "@mantine/core";
 import { HiMenu } from "react-icons/hi";
 import { HiUser } from "react-icons/hi2";
 import DrawerDetailMember from "./drawer_detail_member";
@@ -22,9 +22,11 @@ export default function NavbarDetailMember() {
                justify="center"
                gap="xs"
             >
-               <HiUser size={100} color='white' />
+               <Center>
+                  <Avatar src={'https://i.pravatar.cc/1000?img=25'} alt="it's me" size="xl" />
+               </Center>
                <Text c={'white'} fw={'bold'} fz={25}>Fibra Marcell</Text>
-               <Text c={'white'} fw={'lighter'} fz={15}>Kepala Urusan Pengembangan</Text>
+               <Text c={'white'} fw={'lighter'} fz={15}>Dinas - Kepala Urusan Pengembangan</Text>
             </Stack>
          </LayoutNavbarHome>
          <LayoutDrawer opened={isOpen} title={'Menu'} onClose={() => setOpen(false)}>
