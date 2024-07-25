6872fb51b28c02f45bff4c58255f274039386c1a
lukman <lukman@lukmans-MacBook-Air.local>
2024-07-25 10:07:26 +0800

    feat : add api

---
+++ src/app/api/auth/get-user-by-cookies/route.ts
@@ -0,0 +1,18 @@
+import { prisma, pwd_key_config } from "@/module/_global";
+import { unsealData } from "iron-session";
+import { cookies } from "next/headers";
+
+export async function GET() {
+  const sessionCookie = cookies().get("sessionCookie");
+  const userId = await unsealData(sessionCookie!.value, {
+    password: pwd_key_config,
+  });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: String(userId),
+    },
+  });
+
+  return Response.json(user);
+}
