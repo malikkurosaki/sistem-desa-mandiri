bc3f356e810bbb8e069eb3cf23bb1446019c9f61
amel <amel@amel.local>
2024-07-29 15:11:45 +0800

    upd: log user
    Deskripsi:
    - membuat fungsi create log uer
    
    No Issues

--- src/module/user/index.ts
+++ src/module/user/index.ts
@@ -1,7 +1,9 @@
 import { apiUser } from "./api/api_user";
+import createLogUser from "./log/fun/createLogUser";
 import ViewEditProfile from "./profile/view/view_edit_profile";
 import ViewProfile from "./profile/view/view_profile";
 
 export { ViewProfile };
 export { ViewEditProfile };
 export { apiUser };
+export { createLogUser };
