fcbc136d41c6720385b1cd0397c1fbbedb58947f
lukman <lukman@lukmans-MacBook-Air.local>
2024-07-29 13:52:46 +0800

    api : update announcement

--- src/module/announcement/api/get/getAllAnnouncement.ts
+++ src/module/announcement/api/get/getAllAnnouncement.ts
@@ -2,5 +2,8 @@ import { NextRequest } from "next/server";
 
 export async function getAllAnnouncement(req: NextRequest) {
   try {
+    return Response.json({
+      success: true,
+    });
   } catch (error) {}
 }
