52e9052bb74dcb9ffff8e1a6561a6a3a80093a06
lukman <lukman@lukmans-MacBook-Air.local>
2024-07-29 15:28:45 +0800

    api : update announcement

--- src/module/announcement/api/get/getOneAnnouncement.ts
+++ src/module/announcement/api/get/getOneAnnouncement.ts
@@ -3,7 +3,35 @@ import { NextRequest } from "next/server";
 
 export async function getOneAnnouncement(req: NextRequest) {
   try {
+    const searchParams = req.nextUrl.searchParams;
+    const announcementId = searchParams.get("announcementId");
+    const announcement = await prisma.annoucement.findUnique({
+      where: {
+        id: String(announcementId),
+      },
+      select: {
+        id: true,
+        title: true,
+        desc: true,
+      },
+    });
+    const announcementMember = await prisma.annoucementMember.findMany({
+      where: {
+        idAnnoucement: String(announcementId),
+      },
+      select: {
+        idAnnoucement: true,
+        idGroup: true,
+        idDivision: true,
+      },
+    });
 
+    return Response.json({ announcement, announcementMember });
   } catch (error) {
+    console.error(error);
+    return Response.json(
+      { message: "Internal Server Error", success: false },
+      { status: 500 }
+    );
   }
 }
--- src/module/announcement/api/post/deleteAnnouncement.ts
+++ src/module/announcement/api/post/deleteAnnouncement.ts
@@ -1,7 +1,27 @@
+import { prisma } from "@/module/_global";
 import { NextRequest } from "next/server";
 
 export async function deleteAnnouncement(req: NextRequest) {
-    try {
-    } catch (error) {}
+  try {
+    const data = await req.json();
+    const update = await prisma.annoucement.update({
+      where: {
+        id: data.id,
+      },
+      data: {
+        isActive: false,
+      },
+    });
+
+    return Response.json(
+      { success: true, message: "Sukses Delete Announcement" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      { message: "Internal Server Error", success: false },
+      { status: 500 }
+    );
   }
-  
+}
