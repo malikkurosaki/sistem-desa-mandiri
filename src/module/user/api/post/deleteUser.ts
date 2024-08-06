a1f92392064030cb3b0c52d5fb1f16eceace327e
lukman <lukman@lukmans-MacBook-Air.local>
2024-08-06 10:35:38 +0800

    feat : update api member user

--- src/module/user/api/post/deleteUser.ts
+++ src/module/user/api/post/deleteUser.ts
@@ -1,18 +1,22 @@
 import { createLogUser } from '@/module/user';
 import { prisma } from "@/module/_global";
 import { NextRequest } from "next/server";
+import { revalidatePath } from 'next/cache';
 
 export async function deleteUser(req: NextRequest) {
   try {
     const data = await req.json();
+    const active = data.isActive;
     const update = await prisma.user.update({
       where: {
         id: data.id,
       },
       data: {
-        isActive: false,
+        isActive: !active,
       },
     });
+
+    revalidatePath("/member");
 
     // create log user
     const log = await createLogUser({ act: 'DELETE', desc: 'User menghapus data user', table: 'user', data: update.id })
