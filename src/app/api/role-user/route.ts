8c25532dbb7c0d0a78b51c8383b6d2e407cf849f
amel <amel@amel.local>
2024-08-13 10:47:46 +0800

    fix: api role user
    Deskripsi:
    - kondisi where select data
    - notifikasi message
    
    No Issues

--- src/app/api/role-user/route.ts
+++ src/app/api/role-user/route.ts
@@ -9,13 +9,16 @@ export async function GET(request: Request) {
             return NextResponse.json({ success: false, message: "Anda harus login untuk mengakses ini" }, { status: 401 });
         }
         const res = await prisma.userRole.findMany({
+            where: {
+                isActive: true
+            },
             select: {
                 id: true,
                 name: true,
             },
         });
 
-        return NextResponse.json({ success: true, message: "Berhasil role user", data: res, }, { status: 200 });
+        return NextResponse.json({ success: true, message: "Berhasil mendapatkan role user", data: res, }, { status: 200 });
     } catch (error) {
         console.error(error);
         return NextResponse.json({ success: false, message: "Gagal mendapatkan role user, coba lagi nanti", reason: (error as Error).message, }, { status: 500 });
