9eb2b1e4af0161acd32afef420ea12f0c4fef40a
amel <amel@amel.local>
2024-08-29 11:46:57 +0800

    upd: note note

--- src/module/home/lib/type_home.ts
+++ src/module/home/lib/type_home.ts
@@ -35,3 +35,31 @@ export interface IDataHomeDiskusi {
    date: string
    user: string
 }
+
+
+
+// NOTE :: CARA LAIN BUAT INTERFACE PAKE PRISMA
+// type User = {} & Prisma.UserUncheckedCreateInput
+// type User2 = {} & Prisma.UserGetPayload<{select: {id: true, name: true}}>
+
+// const user = {} as User
+// const user2 = {} as User2
+
+
+
+// NOTE :: CARA LAIN BUAT NYIMPEN DATA PAKE STATIC (LAYOUT>>CHILDREN) CUMA BISA CLIENT TO CLIENT ATAU SERVER TO SERVER
+// class Data {
+//    static nama = ""
+//    static set(val: string){
+//       Data.nama = val
+//    }
+// }
+
+// class Apa {
+//    nama = ""
+//    constructor(nama: string){
+//       this.nama = nama
+//    }
+// }
+
+// const aa = new Apa("aa")
