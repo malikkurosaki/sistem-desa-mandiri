611de86c09d15a152d410bca26ead75f46562b36
amel <amel@amel.local>
2024-08-30 11:13:37 +0800

    upd: auth
    Deskripsi:
    - update log user pada edit profile, login, dan logout
    
    No Issues

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,6 +1,9 @@
+import { createLogUser } from "@/module/user";
 import { cookies } from "next/headers";
 
 export async function DELETE() {
+    const log = await createLogUser({ act: 'LOGOUT', desc: 'User keluar dari program', table: 'user', data: '' })
+
     cookies().delete('sessionCookieSDM')
 
     return Response.json({ success: true })
