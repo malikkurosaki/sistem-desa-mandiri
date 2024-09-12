4d2516b9b0d8b99ea80cd9ec2af618b75c03a129
amel <amel@amel.local>
2024-09-12 17:27:48 +0800

    upd: palet tema warna
    Deskripsi:
    - update tb database
    - update seeder
    
    No Issues

--- src/module/seeder/index.ts
+++ src/module/seeder/index.ts
@@ -5,5 +5,6 @@ import seederUser from "./data/user.json";
 import seederDesa from "./data/desa.json";
 import seederGroup from "./data/group.json";
 import seederPosition from "./data/position.json";
+import seederTheme from "./data/theme.json";
 
-export { seederAdminRole, seederAdmin, seederDesa, seederGroup, seederPosition, seederUserRole, seederUser, }
+export { seederAdminRole, seederAdmin, seederDesa, seederGroup, seederPosition, seederUserRole, seederUser, seederTheme }
