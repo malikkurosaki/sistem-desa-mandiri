79a487e356355387432f26c7883259644bc6d72d
amel <amel@amel.local>
2024-09-25 17:36:08 +0800

    fix : Deskripsi: - laporan divisi grup - order by divisi
    No Issues

--- src/app/api/division/route.ts
+++ src/app/api/division/route.ts
@@ -77,6 +77,9 @@ export async function GET(request: Request) {
                   idUser: true
                }
             }
+         },
+         orderBy: {
+            createdAt: 'desc'
          }
       });
 
