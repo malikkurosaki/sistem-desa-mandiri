bde7a136e2d131f6d4056ee543869887698a5c6f
amel <amel@amel.local>
2024-10-01 11:44:51 +0800

    upd: realtime
    deskripsi:
    - coba realtime
    - api version
    
    No Issues

---
+++ src/app/api/version-app/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+   try {
+      return NextResponse.json({ success: true, version: "0.1" }, { status: 200 });
+   } catch (error) {
+      console.error(error);
+      return NextResponse.json({ success: false, version: "Gagal mendapatkan version, coba lagi nanti", reason: (error as Error).message, }, { status: 500 });
+   }
+}
