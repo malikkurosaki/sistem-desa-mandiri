a61cd084b45b08dd6cdb9839964a8f8cf694ef2f
amel <amel@amel.local>
2024-10-09 12:25:47 +0800

    upd: banner
    Deskripsi:
    - pengaplikasian api banner pada halaman home
    - pengaplikasian api banner pada halaman detail divisi
    
    No Issues

--- src/module/home/ui/carosole.tsx
+++ src/module/home/ui/carosole.tsx
@@ -1,13 +1,46 @@
 'use client'
-import React, { useRef } from 'react';
-import { Carousel } from '@mantine/carousel';
-import { TEMA, WARNA } from '@/module/_global';
-import Autoplay from 'embla-carousel-autoplay';
-import { Flex, Text } from '@mantine/core';
+import { TEMA } from '@/module/_global';
+import { funGetAllBanner, IDataBanner } from '@/module/banner';
 import { useHookstate } from '@hookstate/core';
+import { Carousel } from '@mantine/carousel';
+import { Flex, Image, Text } from '@mantine/core';
+import { useShallowEffect } from '@mantine/hooks';
+import Autoplay from 'embla-carousel-autoplay';
+import { useRef, useState } from 'react';
+import toast from 'react-hot-toast';
+import { funGetHome } from '../lib/api_home';
 export default function Carosole() {
   const autoplay = useRef(Autoplay({ delay: 5000 }));
   const tema = useHookstate(TEMA)
+  const [isDesa, setDesa] = useState("")
+  const [isData, setData] = useState<IDataBanner[]>([])
+
+
+  const fetchData = async () => {
+    try {
+      const res_banner = await funGetAllBanner()
+      if (res_banner.success) {
+        setData(res_banner.data)
+      } else {
+        toast.error(res_banner.message);
+      }
+      const response = await funGetHome('?cat=header')
+      if (response.success) {
+        setDesa(response.data.village)
+      } else {
+        toast.error(response.message);
+      }
+    } catch (error) {
+      toast.error("Gagal mendapatkan data, coba lagi nanti");
+      console.error(error);
+    }
+  };
+
+
+  useShallowEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <Carousel
@@ -17,21 +50,22 @@ export default function Carosole() {
         onMouseEnter={autoplay.current.stop}
         onMouseLeave={autoplay.current.reset}
       >
-        <Carousel.Slide bg={tema.get().utama} style={{ borderRadius: 10 }}>
-          <Flex justify={'center'} h={"100%"} align={'center'}>
-          <Text c={"white"}>INFORMASI DARMASABA</Text>
-          </Flex>
-        </Carousel.Slide>
-        <Carousel.Slide bg={tema.get().utama} style={{ borderRadius: 10 }}>
-          <Flex justify={'center'} h={"100%"} align={'center'}>
-          <Text c={"white"}>INFORMASI DARMASABA</Text>
-          </Flex>
-        </Carousel.Slide>
-        <Carousel.Slide bg={tema.get().utama} style={{ borderRadius: 10 }}>
-          <Flex justify={'center'} h={"100%"} align={'center'}>
-          <Text c={"white"}>INFORMASI DARMASABA</Text>
-          </Flex>
-        </Carousel.Slide>
+        {
+          isData.length > 0 ?
+            isData.map((item, index) => (
+              <Carousel.Slide key={index} bg={tema.get().utama} style={{ borderRadius: 10 }}>
+                <Image alt={item.title} h={"100%"} style={{ borderRadius: 10 }} src={`https://wibu-storage.wibudev.com/api/files/${item.image}`} />
+              </Carousel.Slide>
+            ))
+            :
+            [...Array(3)].map((_, index) => (
+              <Carousel.Slide key={index} bg={tema.get().utama} style={{ borderRadius: 10 }}>
+                <Flex justify={'center'} h={"100%"} align={'center'}>
+                  <Text c={"white"}>INFORMASI {isDesa.toUpperCase()}</Text>
+                </Flex>
+              </Carousel.Slide>
+            ))
+        }
       </Carousel>
     </>
   );
